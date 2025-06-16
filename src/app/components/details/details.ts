3fb9f35aa7abd74ccc0cf7f49be1b88c49490f6a
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2025-06-16 15:20:57 +0300

    feat(details): add navigation buttons and improve product details handling
    - Add back, previous, and next buttons to navigate between products
    - Implement navigation logic in component
    - Switch from snapshot to observable for route parameter changes
    - Store product IDs array for navigation purposes

--- src/app/components/details/details.ts
+++ src/app/components/details/details.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../../models/iproduct';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { StaticProducts } from '../../services/static-products';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-details',
@@ -12,14 +13,45 @@ import { StaticProducts } from '../../services/static-products';
 export class Details implements OnInit {
   currentId: number = 0;
   CurrentProduct: IProduct | undefined = undefined;
-
+  idsArr: number[] = [];
+  currentIdIndex: number = 0;
   constructor(
     private _activatedRoute: ActivatedRoute,
-    private _staticProducts: StaticProducts
-  ) {}
+    private _staticProducts: StaticProducts,
+    private _location: Location,
+    private _router: Router
+  ) {
+    this.idsArr = this._staticProducts.getAllProducts().map((p) => p.id);
+    this.currentIdIndex = this.idsArr.indexOf(this.currentId);
+  }
 
   ngOnInit(): void {
-    this.currentId = Number(this._activatedRoute.snapshot.paramMap.get('id'));
-    this.CurrentProduct = this._staticProducts.getProductById(this.currentId);
+    this._activatedRoute.paramMap.subscribe((param) => {
+      this.currentId = Number(param.get('id'));
+      this.CurrentProduct = this._staticProducts.getProductById(this.currentId);
+    });
+
+    // this.currentId = Number(this._activatedRoute.snapshot.paramMap.get('id'));
+    // this.CurrentProduct = this._staticProducts.getProductById(this.currentId);
+  }
+
+  goBack() {
+    this._location.back();
+  }
+  goNext() {
+    this.currentIdIndex = this.idsArr.indexOf(this.currentId);
+    if (this.currentIdIndex !== this.idsArr.length - 1) {
+      this._router.navigateByUrl(
+        `details/${this.idsArr[this.currentIdIndex + 1]}`
+      );
+    }
+  }
+  goPrev() {
+    this.currentIdIndex = this.idsArr.indexOf(this.currentId);
+    if (this.currentIdIndex !== 0) {
+      this._router.navigateByUrl(
+        `details/${this.idsArr[this.currentIdIndex - 1]}`
+      );
+    }
   }
 }
